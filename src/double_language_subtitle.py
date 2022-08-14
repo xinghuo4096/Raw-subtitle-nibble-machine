fc76cdd1506305f3e2d8ad983e47796a4c7c7ab0
xinghuo4096 <262066859@qq.com>
2022-08-14 18:19:33 +0800

    更新例子

--- src/double_language_subtitle.py
+++ src/double_language_subtitle.py
@@ -2,7 +2,7 @@ from copy import deepcopy
 import datetime
 import re
 
-from Srt import detect_code, load_srt_fromfile, reidnex, save_srt
+from Srt import detect_code, load_srt_fromfile, merge_ass_tofile, reidnex, save_srt
 
 from translator import (GoogleFree, Media,  SubBlock, Subtitle, Translator, languages,
                         save_file)
@@ -81,47 +81,71 @@ def make_double_lanague_subtitle(media: str,
     save_file(dict_text, '\n'.join(strlist))
 
 
-CLEAR_TEXT_MARK1 = r'\[[^\]]+?\]'
-CLEAR_TEXT_MARK2 = r'[]'
-CLEAR_TEXT_MARK3 = r''
+def clear_subtitle():
+    '''
+    清除字幕里的特殊字符。
+    '''
+    CLEAR_TEXT_MARK1 = r'\[[^\]]+?\]'
+    CLEAR_TEXT_MARK2 = r'[]'
+    CLEAR_TEXT_MARK3 = r''
 
-CLEAR_TEXT_MARK4 = r'<i>.+?music.+?</i>'
-str1 = re.search(CLEAR_TEXT_MARK4, '<i> upbeat music playing </i>')
+    CLEAR_TEXT_MARK4 = r'<i>.+?music.+?</i>'
+    str1 = re.search(CLEAR_TEXT_MARK4, '<i> upbeat music playing </i>')
+
+    for i in range(1, 11):
+        srtname = f'z:/tests/1/The.Man.Who.Fell.to.Earth.S01E{i:0>2}.1080p.WEBRip.x265-RARBG.srt'
+        srtname_en = f'z:/tests/1/The.Man.Who.Fell.to.Earth.S01E{i:0>2}.1080p.WEBRip.x265-RARBG.en.srt'
+        movie1 = Media(f'movie {i}')
+        movie1.add_subtitle('en', srtname)
+        sub = movie1.subtitles[0]
+        assert isinstance(sub, Subtitle)
+
+        for item in sub.subblocks:
+            str1 = item.text
+            str1 = re.sub(CLEAR_TEXT_MARK1, '', str1)
+            str1 = re.sub(CLEAR_TEXT_MARK4, '', str1)
+            str1 = str1.replace(r'{\an8}', '')
+            str1 = str1.replace('<i>', '')
+            str1 = str1.replace('</i>', '')
+            str1 = str1.strip()
+            if not re.search(r'\w+', str1):
+                str1 = ''
+            item.text = str1
+
+        sub2 = list()
+        for item in sub.subblocks:
+            if item.text:
+                sub2.append(item)
+        reidnex(sub2)
+
+        save_srt(srtname_en, sub2)
 
 
-for i in range(1, 11):
-    srtname = f'z:/tests/1/The.Man.Who.Fell.to.Earth.S01E{i:0>2}.1080p.WEBRip.x265-RARBG.srt'
-    srtname_en = f'z:/tests/1/The.Man.Who.Fell.to.Earth.S01E{i:0>2}.1080p.WEBRip.x265-RARBG.en.srt'
-    movie1 = Media(f'movie {i}')
-    movie1.add_subtitle('en', srtname)
-    sub = movie1.subtitles[0]
-    assert isinstance(sub, Subtitle)
+def main_batch():
+    '''
+    批处理
+    '''
+    for i in range(1, 9):
+        print(i)
+        fname = f'z:/tests/a/test.S01E{i:0>2}.'
 
-    for item in sub.subblocks:
-        str1 = item.text
-        str1 = re.sub(CLEAR_TEXT_MARK1, '', str1)
-        str1 = re.sub(CLEAR_TEXT_MARK4, '', str1)
-        str1 = str1.replace(r'{\an8}', '')
-        str1 = str1.replace('<i>', '')
-        str1 = str1.replace('</i>', '')
-        str1 = str1.strip()
-        if not re.search(r'\w+', str1):
-            str1 = ''
-        item.text = str1
-
-    sub2 = list()
-    for item in sub.subblocks:
-        if item.text:
-            sub2.append(item)
-    reidnex(sub2)
-
-    save_srt(srtname_en, sub2)
+        make_double_lanague_subtitle(media=f'movie {i}',
+                                     from_sub=f'{fname}.en.srt',
+                                     to_sub=f'{fname}.cn.srt',
+                                     err_text=f'{fname}.err.txt',
+                                     dict_text=f'{fname}.dict.txt')
 
 
-for i in range(1, 11):
-    print(i)
-    make_double_lanague_subtitle(media=f'movie {i}',
-                                 from_sub=f'z:/tests/1/The.Man.Who.Fell.to.Earth.S01E{i:0>2}.1080p.WEBRip.x265-RARBG.en.srt',
-                                 to_sub=f'z:/tests/1/The.Man.Who.Fell.to.Earth.S01E{i:0>2}.1080p.WEBRip.x265-RARBG.cn.srt',
-                                 err_text=f'z:/tests/1/The.Man.Who.Fell.to.Earth.S01E{i:0>2}.1080p.WEBRip.x265-RARBG.err.txt',
-                                 dict_text=f'z:/tests/1/The.Man.Who.Fell.to.Earth.S01E{i:0>2}.1080p.WEBRip.x265-RARBG.srt.dict.txt')
+def mymain():
+    '''
+    单个处理
+    '''
+    fname = 'z:/tests/a/test'
+    make_double_lanague_subtitle(media=f'movie {fname}',
+                                 from_sub=f'{fname}.en.srt',
+                                 to_sub=f'{fname}.cn.srt',
+                                 err_text=f'{fname}.err.txt',
+                                 dict_text=f'{fname}.dict.txt')
+
+
+main_batch()
